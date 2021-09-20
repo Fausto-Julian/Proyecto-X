57a328ff13c7c1438af401a4db6b1ebc099e140d
Julian Fausto <julianfausto09@gmail.com>
2021-09-20 00:23:11 -0300

    Movement and Arack
    The player movement was added and the base of the mele attack was made and also enemies to test the mele attack.

---
+++ Proyecto X - V 0.1/Assets/_main/Scripts/player/atack1Script.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class atack1Script : MonoBehaviour
+{
+    [SerializeField] private float timeLifeBullet = 1f;
+    [SerializeField] private float damage = 10f;
+
+    private float currentTime;
+
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime >= timeLifeBullet)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
--- Proyecto X - V 0.1/Assets/_main/Scripts/player/playerController.cs
+++ Proyecto X - V 0.1/Assets/_main/Scripts/player/playerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     [SerializeField] private float speed;
-
     [SerializeField] private Camera cam;
+
+    [SerializeField] private Transform spawnPointAtack1;
+    [SerializeField] private GameObject atack1;
 
     private Rigidbody2D rb;
 
@@ -20,16 +22,24 @@ public class playerController : MonoBehaviour
 
     private void Update()
     {
+
+        float t = Time.deltaTime;
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * speed * t);
 
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(atack1, spawnPointAtack1.position, spawnPointAtack1.rotation);
+        }
     }
 
 }
